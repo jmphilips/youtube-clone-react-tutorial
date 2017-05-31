f838ad3b5add01fffeda2a1fd132527122028fe9
Joshua Philips <jmichaelphilips@gmail.com>
2017-05-30 21:01:18 -0500

    First commit.

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyC9lSVQZkJgFIktNB50TrfuVfawBjD2tjc';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'Stanley Cup 2017'}, (videos) => {
+      this.setState({ videos: videos, selectedVideo: videos[0] })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} 
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
